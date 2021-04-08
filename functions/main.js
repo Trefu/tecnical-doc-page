6fac785e82084a0484b79a2aa151b117abf2a812
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-04-08 05:55:16 -0300

    song, welcome div

--- functions/main.js
+++ functions/main.js
@@ -1,1 +1,21 @@
 console.log("ASDASD")
+const song = new Audio("sounds/rest.mp3")
+document.addEventListener("DOMcontentLoaded", e => song.play())
+
+
+
+const fader = function () {
+    let divImage = document.querySelector(".over")
+    let div = document.querySelector(".hider")
+    divImage.remove()
+    div.classList.remove("hider")
+    play()
+}
+
+
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.volume = 0.1
+    audio.play();
+}
