5c61a2af5fa1f34a2fe15c8314d296c4445db2ff
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-04-09 18:04:23 -0300

    var rojo, fixes mobile responsive, y barra nav

--- functions/main.js
+++ functions/main.js
@@ -1,20 +1,18 @@
-console.log("ASDASD")
 const song = new Audio("sounds/rest.mp3")
 document.addEventListener("DOMcontentLoaded", e => song.play())
 
-
-
-const fader = function () {
+const fader = () => {
     let divImage = document.querySelector(".over")
     let div = document.querySelector(".hider")
     divImage.remove()
     div.classList.remove("hider")
+
+
     play()
 }
 
 
-
-function play() {
+const play = () => {
     var audio = document.getElementById("audio");
     audio.volume = 0.1
     audio.play();
